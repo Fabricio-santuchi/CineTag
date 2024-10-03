69576398bc3bde1b856477d1a2ebc675585713ef
Fabricio santuchi <131098765+Fabricio-santuchi@users.noreply.github.com>
2024-10-03 20:37:13 -0300

    sv

--- src/styles/ContainerCards_style/index.jsx
+++ src/styles/ContainerCards_style/index.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const ContainerDeCards = styled.section`
   display: flex;
   justify-content: space-around;
+  gap: 20px;
 `;
 
 export default ContainerDeCards;
